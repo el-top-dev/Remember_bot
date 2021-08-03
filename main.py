262ff7e8aa03a93622780751861d46e01e848554
Alex <elips61002@gmail.com>
2021-08-04 02:04:22 +1000

    try exemple

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 from settings.settings import API_TOKEN
-import aiogram
+from aiogram import Bot, Dispatcher, executor, types
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
